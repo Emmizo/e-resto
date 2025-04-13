c3f6b700baab17bc5c0ac8ac3171e5977f64d2e8
Emmizo <emmizokwizera@gmail.com>
2025-04-13 18:12:55 +0200

    new changes

--- app/Http/Controllers/EventsController.php
+++ app/Http/Controllers/EventsController.php
@@ -14,13 +14,7 @@ class EventsController extends Controller
         return view('manage-events.index');
     }
 
-    /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
+    
 
     /**
      * Store a newly created resource in storage.
